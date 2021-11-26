396b5648a54494ab8e47df0ed7db464502b8cd78
Rahman Elsirafy <rahman2408@gmail.com>
2021-11-25 22:02:16 -0500

    commit tree a2bc611d4816961dead80ced9d16a389539cbe8e

--- src/App.js
+++ src/App.js
@@ -1,1 +1,23 @@
 // create your App component here
+import React, {Component} from 'react';
+ 
+export default class App extends Component {
+
+  state = {
+    astroPeople: []
+  }
+  render() {
+    return <div>{this.state.astroPeople.map((person) => <li>{person.name}</li>)}</div>
+  }
+
+  componentDidMount() {
+    fetch("http://api.open-notify.org/astros.json")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data.people)
+      this.setState({
+        astroPeople: data.people
+      })
+    })
+  }
+}
